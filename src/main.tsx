6d3beb44d690a0756986de6ec3f1e085531e63c2
Shubham Jha <shubhamjha271005@gmail.com>
2026-09-08 02:55:36 +0530

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// @ts-expect-error Leaflet's CSS package does not provide TypeScript declarations.
 import 'leaflet/dist/leaflet.css'
+// @ts-expect-error CSS imports are handled by the bundler and have no TypeScript declarations.
 import './styles.css'
 import App from './App'
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
